e6a80b0e8a3d6be4e99d93d89fbe881a0cfb9d0e
Arthur <example@gmail.com>
2025-01-24 16:51:32 +0200

    first commit

---
+++ JS/BaseJS/train2.js
@@ -0,0 +1,29 @@
+// правила и типы названия переменных
+
+const vehicleBodyWidth = 10000;
+const vehicleBodyHeight = 40000;
+
+// const vehicleBodyWidth = 10000, const vehicleBodyHeight = 40000; - так тоже можно
+
+console.log(
+  "Ширина кузова автомобиля: " +
+    vehicleBodyWidth +
+    ", длинна: " +
+    vehicleBodyHeight
+);
+// console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ", длинна: " + vehicleBodyHeight);
+// ЛУЧШЕ ВСЕГО давать переменные особенными именами
+
+// ['vruru', 'efefef', 'dvfvs'].map(a => ) // проводим с каждым 'сотрудником' действие
+
+// Запрос
+data;
+response;
+
+// можно обозначать переменные:
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+const COLOR_RED = "#F00";
